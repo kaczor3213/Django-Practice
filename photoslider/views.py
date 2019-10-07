eb222e474757a8ce933bc58fb6d26a9c3d9f1c8e
kaczor <przemyslaw.markiewicz@besidethepark.com>
2019-10-07 22:25:54 +0200

    asshole

--- photoslider/views.py
+++ photoslider/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, Http404
 from django.views import View
 from os import listdir
+import os
 from os.path import isfile, join
 
 class SliderView(View):
     def get(self, request):
-        #images = [f for f in listdir('./images') if isfile(join('./images', f))]
-        return render(request, 'photoslider/slider.html', {})
+        images = [f for f in listdir('photoslider/static/images/') if isfile(join('photoslider/static/images/', f))]
+        context = {
+            'images' : images,
+        }
+        return render(request, 'photoslider/slider.html', context)
     def post(self, request):
         pass
